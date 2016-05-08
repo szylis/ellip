0760fd058c444e983b3595e2b8b2d52bf7ef3115
Szymon <szymonlis@yahoo.com>
2016-05-08 17:16:36 +0100

    MagnitudeAndPhaseAddedAndTested

--- ComplexNumber.h
+++ ComplexNumber.h
@@ -1,6 +1,13 @@
 #ifndef ComplexNumber_h
 #define ComplexNumber_h
 
+
+#ifndef MY_PI
+#define MY_PI 3.141592653589793238
+#endif 
+
+//#include <math.h>
+
 struct COMPLEX {
 	double real, imag;
 
@@ -27,6 +34,47 @@ public:
 
 	COMPLEX Conjugate(COMPLEX complex) const {
 		return COMPLEX(complex.real, -1.0*complex.imag);
+	}
+
+	double Magnitude(COMPLEX complex) const {
+		return  sqrt(complex.real*complex.real + complex.imag*complex.imag);
+	}
+
+	double Phase(COMPLEX complex) const {
+		if(complex.real == 0 && complex.imag == 0)
+			return 0;
+
+		if(complex.real == 0 && complex.imag > 0)
+			return 90 * (MY_PI/180);
+
+		if(complex.real == 0 && complex.imag < 0)
+			return -90 * (MY_PI/180);
+
+		return atan(complex.imag/complex.real);
+	}
+
+	COMPLEX Addition() {
+		return COMPLEX();
+	}
+
+	COMPLEX Subtraction() {
+		return COMPLEX();
+	}
+
+	COMPLEX Multiplication() {
+		return COMPLEX ();
+	}
+
+	COMPLEX Division() {
+		return COMPLEX ();
+	}
+
+	COMPLEX Reciprocal() {
+		return COMPLEX ();
+	}
+
+	COMPLEX SquareRoot() {
+		return COMPLEX ();
 	}
 };
 
--- complexTest.cpp
+++ complexTest.cpp
@@ -10,16 +10,36 @@ public:
 
 TEST_F(ComplexNumberOperations, EqualityOfComplex) {
 
-	COMPLEX complex(5.0, 3.0);
-
-	ASSERT_THAT(complexNumber.Equality(complex).real, Eq(5.0));
-	ASSERT_THAT(complexNumber.Equality(complex).imag, Eq(3.0));
+	ASSERT_THAT(complexNumber.Equality(COMPLEX(5.0, 3.0)).real, Eq(5.0));
+	ASSERT_THAT(complexNumber.Equality(COMPLEX(5.0, 3.0)).imag, Eq(3.0));
 }
 
 TEST_F(ComplexNumberOperations, ConjugateOfComplex) {
 	
-	COMPLEX complex(5.0, 3.0);
+	ASSERT_THAT(complexNumber.Conjugate(COMPLEX(5.0, 3.0)).real, Eq(5.0));
+	ASSERT_THAT(complexNumber.Conjugate(COMPLEX(5.0, 3.0)).imag, Eq(-3.0));
+}
+
+TEST_F(ComplexNumberOperations, ConjugateOfReal) {
 	
-	ASSERT_THAT(complexNumber.Conjugate(complex).real, Eq(5.0));
-	ASSERT_THAT(complexNumber.Conjugate(complex).imag, Eq(-3.0));
+	ASSERT_THAT(complexNumber.Conjugate(COMPLEX(-5.0)).real, Eq(-5.0));
+	ASSERT_THAT(complexNumber.Conjugate(COMPLEX(-5.0)).imag, Eq(0.0));
+}
+
+TEST_F(ComplexNumberOperations, ConjugateOfImag) {
+	
+	ASSERT_THAT(complexNumber.Conjugate(COMPLEX(0.0, -7.3)).real, Eq(0.0));
+	ASSERT_THAT(complexNumber.Conjugate(COMPLEX(0.0, -7.3)).imag, Eq(7.3));
+}
+
+TEST_F(ComplexNumberOperations, MagnitudeOfComplex) {
+
+	ASSERT_THAT(complexNumber.Magnitude(COMPLEX(3.2, 4.5)), Eq(sqrt(30.49)));
+}
+
+TEST_F(ComplexNumberOperations, PhaseOfComplex) {
+
+	ASSERT_THAT(complexNumber.Phase(COMPLEX(2.0, 4.0)), Eq(atan(2.0)));
+	ASSERT_THAT(complexNumber.Phase(COMPLEX(0.0, 4.0)), Eq(90.0 * (MY_PI/180)));
+	ASSERT_THAT(complexNumber.Phase(COMPLEX(0.0, -2.0)), Eq(-90 * (MY_PI/180)));
 }
