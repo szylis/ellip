1429a059830c241fff5f6a7d08d807f144be3d6a
Szymon <szymonlis@yahoo.com>
2016-05-09 22:58:59 +0100

    FinalRayClassAndTestUnit

--- Ray.h
+++ Ray.h
@@ -8,48 +8,117 @@ class Ray {
 
 public:
 
+//constructor prototypes
 	Ray();
 	Ray(double);
 	Ray(double, double);
+	Ray(double, double, double);
+	Ray(double, double, double, double);
+	Ray(double, double, double, double, bool);
+	Ray(double, double, double, double, bool, int); 
 	~Ray();
 
-	double GetPropagationAngle();
-	void SetPropagationAngle(double);
+//methods
+	double GetRayAngle();
+	void SetRayAngle(double);
 
 	double GetRayAmplitude();
 	void SetRayAmplitude(double);
+
+	double GetRayPhase();
+	void SetRayPhase(double);
+
+	double GetRayWavelength();
+	void SetRayWavelength(double);
+
+	bool GetRayDirection();
+	void SetRayDirection(bool);
+
+	int GetRayLayer();
+	void SetRayLayer(int);
 
 private:
 	
 	double angle;
 	double amplitude;
-
+	double phase;
+	double wavelength;
+	bool direction;
+	int layer;
 };
 
 //constructors of ray
 Ray::Ray() {
 	angle = 0.0;
 	amplitude = 1.0;
+	phase = 0.0;
+	wavelength = 1.0;
+	direction = true;
+	layer = 0;
 }
 
 Ray::Ray(double x) {
 	angle = x;
 	amplitude = 1.0;
+	phase = 0.0;
+	wavelength = 1.0;
+	direction = true;
+	layer = 0;
 }
 
 Ray::Ray(double x, double y) {
 	angle = x;
 	amplitude = y;
+	phase = 0.0;
+	wavelength = 1.0;
+	direction = true;
+	layer = 0;
+}
+
+Ray::Ray(double x, double y, double p) {
+	angle = x;
+	amplitude = y;
+	phase = p;
+	wavelength = 1.0;
+	direction = true;
+	layer = 0;
+}
+
+Ray::Ray(double x, double y, double p, double w) {
+	angle = x;
+	amplitude = y;
+	phase = p;
+	wavelength = w;
+	direction = true;
+	layer = 0;
+}
+
+Ray::Ray(double x, double y, double p, double w, bool d) {
+	angle = x;
+	amplitude = y;
+	phase = p;
+	wavelength = w;
+	direction = d;
+	layer = 0;
+}
+
+Ray::Ray(double x, double y, double p, double w, bool d, int l) {
+	angle = x;
+	amplitude = y;
+	phase = p;
+	wavelength = w;
+	direction = d;
+	layer = l;
 }
 
 Ray::~Ray() {
 }
 
-double Ray::GetPropagationAngle() {
+double Ray::GetRayAngle() {
 	return angle;
 }
 
-void Ray::SetPropagationAngle(double x) {
+void Ray::SetRayAngle(double x) {
 	angle = x;
 }
 
@@ -57,11 +126,42 @@ double Ray::GetRayAmplitude() {
 	return amplitude;
 }
 
-void Ray::SetRayAmplitude(double y) {
-	amplitude = y;
+void Ray::SetRayAmplitude(double x) {
+	amplitude = x;
 }
-	
-		
+
+double Ray::GetRayPhase() {
+	return phase;
+}
+
+void Ray::SetRayPhase(double x) {
+	phase = x;
+}
+
+double Ray::GetRayWavelength() {
+	return wavelength;
+}
+
+void Ray::SetRayWavelength(double x) {
+	wavelength = x;
+}
+
+bool Ray::GetRayDirection() {
+	return direction;
+}
+
+void Ray::SetRayDirection(bool x) {
+	direction = x;
+}
+
+int Ray::GetRayLayer() {
+	return layer;
+}
+
+void Ray::SetRayLayer(int x) {
+	layer = x;
+}
+
 
 
 #endif
--- rayTest.cpp
+++ rayTest.cpp
@@ -9,26 +9,108 @@ public:
 	Ray ray;
 };
 
-TEST_F(PropagatedRay, GetPropagationAngle) {
-
-	Ray incRay(47.5, 1.0);
-	ASSERT_DOUBLE_EQ(incRay.GetPropagationAngle(), 47.5);
+TEST_F(PropagatedRay, Constructor_0) {
+	ASSERT_DOUBLE_EQ(ray.GetRayAngle(), 0.0);
+	ASSERT_DOUBLE_EQ(ray.GetRayAmplitude(), 1.0);
+	ASSERT_DOUBLE_EQ(ray.GetRayPhase(), 0.0);
+	ASSERT_DOUBLE_EQ(ray.GetRayWavelength(), 1.0);
+	ASSERT_EQ(ray.GetRayDirection(), true);
+	ASSERT_EQ(ray.GetRayLayer(), 0);
 }
 
-TEST_F(PropagatedRay, SetPropagationAngle) {
+TEST_F(PropagatedRay, Constructor_1) {
 
-	ray.SetPropagationAngle(30.0);
-	ASSERT_DOUBLE_EQ(ray.GetPropagationAngle(), 30.0);
+	Ray laser(30.0);
+	ASSERT_DOUBLE_EQ(laser.GetRayAngle(), 30.0);
+	ASSERT_DOUBLE_EQ(laser.GetRayAmplitude(), 1.0);
+	ASSERT_DOUBLE_EQ(laser.GetRayPhase(), 0.0);
+	ASSERT_DOUBLE_EQ(laser.GetRayWavelength(), 1.0);
+	ASSERT_EQ(laser.GetRayDirection(), true);
+	ASSERT_EQ(laser.GetRayLayer(), 0);
 }
 
-TEST_F(PropagatedRay, GetRayAmplitude) {
 
-	Ray incRay(47.5, 0.88);
-	ASSERT_DOUBLE_EQ(incRay.GetRayAmplitude(), 0.88);
+TEST_F(PropagatedRay, Constructor_2) {
+
+	Ray laser(30.0, 0.6);
+	ASSERT_DOUBLE_EQ(laser.GetRayAngle(), 30.0);
+	ASSERT_DOUBLE_EQ(laser.GetRayAmplitude(), 0.6);
+	ASSERT_DOUBLE_EQ(laser.GetRayPhase(), 0.0);
+	ASSERT_DOUBLE_EQ(laser.GetRayWavelength(), 1.0);
+	ASSERT_EQ(laser.GetRayDirection(), true);
+	ASSERT_EQ(laser.GetRayLayer(), 0);
+}
+
+TEST_F(PropagatedRay, Constructor_3) {
+
+	Ray laser(30.0, 0.6, MY_PI);
+	ASSERT_DOUBLE_EQ(laser.GetRayAngle(), 30.0);
+	ASSERT_DOUBLE_EQ(laser.GetRayAmplitude(), 0.6);
+	ASSERT_DOUBLE_EQ(laser.GetRayPhase(), MY_PI);
+	ASSERT_DOUBLE_EQ(laser.GetRayWavelength(), 1.0);
+	ASSERT_EQ(laser.GetRayDirection(), true);
+	ASSERT_EQ(laser.GetRayLayer(), 0);
+}
+
+TEST_F(PropagatedRay, Constructor_4) {
+
+	Ray laser(30.0, 0.6, MY_PI, 0.632);
+	ASSERT_DOUBLE_EQ(laser.GetRayAngle(), 30.0);
+	ASSERT_DOUBLE_EQ(laser.GetRayAmplitude(), 0.6);
+	ASSERT_DOUBLE_EQ(laser.GetRayPhase(), MY_PI);
+	ASSERT_DOUBLE_EQ(laser.GetRayWavelength(), 0.632);
+	ASSERT_EQ(laser.GetRayDirection(), true);
+	ASSERT_EQ(laser.GetRayLayer(), 0);
+}
+
+TEST_F(PropagatedRay, Constructor_5) {
+
+	Ray laser(30.0, 0.6, MY_PI, 0.632, false);
+	ASSERT_DOUBLE_EQ(laser.GetRayAngle(), 30.0);
+	ASSERT_DOUBLE_EQ(laser.GetRayAmplitude(), 0.6);
+	ASSERT_DOUBLE_EQ(laser.GetRayPhase(), MY_PI);
+	ASSERT_DOUBLE_EQ(laser.GetRayWavelength(), 0.632);
+	ASSERT_EQ(laser.GetRayDirection(), false);
+	ASSERT_EQ(laser.GetRayLayer(), 0);
+}
+
+TEST_F(PropagatedRay, Constructor_6) {
+
+	Ray laser(30.0, 0.6, MY_PI, 0.632, false, 3);
+	ASSERT_DOUBLE_EQ(laser.GetRayAngle(), 30.0);
+	ASSERT_DOUBLE_EQ(laser.GetRayAmplitude(), 0.6);
+	ASSERT_DOUBLE_EQ(laser.GetRayPhase(), MY_PI);
+	ASSERT_DOUBLE_EQ(laser.GetRayWavelength(), 0.632);
+	ASSERT_EQ(laser.GetRayDirection(), false);
+	ASSERT_EQ(laser.GetRayLayer(), 3);
+}
+
+TEST_F(PropagatedRay, SetRayAngle) {
+	ray.SetRayAngle(28.8);
+	ASSERT_DOUBLE_EQ(ray.GetRayAngle(), 28.8);
 }
 
 TEST_F(PropagatedRay, SetRayAmplitude) {
-
 	ray.SetRayAmplitude(0.77);
 	ASSERT_DOUBLE_EQ(ray.GetRayAmplitude(), 0.77);
+}
+
+TEST_F(PropagatedRay, SetRayPhase) {
+	ray.SetRayPhase(MY_PI/2);
+	ASSERT_DOUBLE_EQ(ray.GetRayPhase(), MY_PI/2);
+}
+
+TEST_F(PropagatedRay, SetRayWavelength) {
+	ray.SetRayWavelength(0.350);
+	ASSERT_DOUBLE_EQ(ray.GetRayWavelength(), 0.350);
+}
+
+TEST_F(PropagatedRay, SetRayDirection) {
+	ray.SetRayDirection(false);
+	ASSERT_EQ(ray.GetRayDirection(), false);
+}
+
+TEST_F(PropagatedRay, SetRayLayer) {
+	ray.SetRayLayer(2);
+	ASSERT_EQ(ray.GetRayLayer(), 2);
 }
