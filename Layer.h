ad7564c055fbf1c22fd44da6ea5cf2cd2284ad7f
Szymon <szymonlis@yahoo.com>
2016-05-10 11:43:20 +0100

    CleanUpLayerClass

--- Layer.h
+++ Layer.h
@@ -1,6 +1,19 @@
+//-------------------------------------------------------------------------------------------------------
+//
+// Class holding the physical layer object
+//
+// Layer is characterized by:
+//	- complex refractive index
+//	- thickness
+//	- position number (counting from the top (air -> substrate),
+//		where layer number 0 is assumed to be air)
+// 
+// Author: Szymon Lis	twitter: @drSzymonLis	email: szymonlis@yahoo.com
+//
+//-----------------------------------------------------------------------------------------------------
+
 #ifndef Layer_h
 #define Layer_h
-
 
 #include "ComplexNumber.h"
 
@@ -8,45 +21,64 @@ class Layer {
 
 public:
 
+//constructors
 	Layer();
 	Layer(COMPLEX);
-	Layer(COMPLEX, double);
+	Layer(COMPLEX, int);
+	Layer(COMPLEX, int, double);
 	~Layer();
 
-	COMPLEX GetRefractiveIndex();
+//prototypes
+	COMPLEX GetRefractiveIndex() const;
 	void SetRefractiveIndex(COMPLEX);
 
-	double GetThickness();
+	int GetPosition() const;
+	void SetPosition(int);
+
+	double GetThickness() const;
 	void SetThickness(double);
 
-
+//members
 private:
 
 	COMPLEX refractiveIndex;	//refractive index
 	double thickness;		//thickness of layer
+	int position;			//number of layer on the layer stack
 };
 
 
-//constrtuctors of layers
+//definition of layer constructors
 Layer::Layer() {
-	refractiveIndex = {1.0, 0.0}; 
+	refractiveIndex = {1.0, 0.0};
+	position = 0; 
 	thickness = 0.0;
 }
 
 Layer::Layer(COMPLEX x) {
 	refractiveIndex = x;
+	position = 0;
 	thickness = 0.0;
 }
 
-Layer::Layer(COMPLEX x, double y) {
+Layer::Layer(COMPLEX x, int y) {
 	refractiveIndex = x;
-	thickness = y;
+	position = y;
+	thickness = 0.0;
 }
 
+Layer::Layer(COMPLEX x, int y, double z) {
+	refractiveIndex = x;
+	position = y;
+	thickness = z;
+}
+
+//deconstructor
 Layer::~Layer() {
 }
 
-COMPLEX Layer::GetRefractiveIndex() {	
+
+//definition of methods
+COMPLEX Layer::GetRefractiveIndex() const {
 	return {refractiveIndex.real, refractiveIndex.imag};
 }
 
@@ -54,7 +86,15 @@ void Layer::SetRefractiveIndex(COMPLEX x) {
 	refractiveIndex = x;
 }
 
-double Layer::GetThickness() {
+int Layer::GetPosition() const {
+	return position;
+}
+
+void Layer::SetPosition(int x) {
+	position = x;
+}
+
+double Layer::GetThickness() const {
 	return thickness;
 }
 
--- layerTest.cpp
+++ layerTest.cpp
@@ -12,7 +12,7 @@ public:
 
 TEST_F(OpticLayer, GetRefractiveIndex) {
 
-	Layer glass({1.45, 0.0}, 0.2);
+	Layer glass({1.45, 0.0}, 1, 0.2);
 
 	ASSERT_DOUBLE_EQ(glass.GetRefractiveIndex().real, 1.45);
 	ASSERT_DOUBLE_EQ(glass.GetRefractiveIndex().imag, 0.0);
@@ -26,9 +26,21 @@ TEST_F(OpticLayer, SetRefractiveIndex) {
 	ASSERT_DOUBLE_EQ(layer.GetRefractiveIndex().imag, 0.2);
 }
 
+TEST_F(OpticLayer, GetPositionNumber) {
+
+	Layer glass({1.45, 0.0}, 3, 0.4);
+	ASSERT_EQ(glass.GetPosition(), 3);
+}
+
+TEST_F(OpticLayer, SetPositionNumber) {
+	
+	layer.SetPosition(1);
+	ASSERT_EQ(layer.GetPosition(), 1);
+}
+
 TEST_F(OpticLayer, GetThickness) {
 
-	Layer glass({1.45, 0.0}, 0.2);
+	Layer glass({1.45, 0.0}, 2, 0.2);
 
 	ASSERT_DOUBLE_EQ(glass.GetThickness(), 0.2);
 }
