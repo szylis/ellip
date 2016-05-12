893e825a82f88b1fb6c56ce9cd590cde6b93be71
Szymon <szymonlis@yahoo.com>
2016-05-12 17:08:14 +0100

    Output data are streamed to file

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ using namespace std;
 int main()
 {
 
+
 	//stac of layers
 	Layer air(0, {1.0, 0.0}, 0.0);
 	Layer glass(1, {1.5, 0.0}, 1.0);;
@@ -23,15 +24,25 @@ int main()
 	//interface
 	HitPoint hit;
 
-	printf("Angle:\tRp:\tRs:\t\n");
 
-
-	for(unsigned int i=0; i < 15; i++) 
-	{
-		laser.SetRayAngle(5.0*i);
-		hit.Solve(air, glass, laser, refl, tran);
-		printf("%.5f %.5f %.5f\n", laser.GetRayAngle(), refl.GetRayAmp_p(), refl.GetRayAmp_s());
+	FILE *output;
+	output = fopen("data.out", "w");
+	if (output == NULL) {
+		printf("Error opening file data.out\n");
+		return 0;
 	}
+
+
+	fprintf(output, "Angle:\tRp:\tRs:\t\n");
+
+	for(unsigned int i=0; i <= 90; i++) 
+	{
+		laser.SetRayAngle(i);
+		hit.Solve(air, glass, laser, refl, tran);
+		fprintf(output, "%7.3f %7.5f %7.5f\n", laser.GetRayAngle(), refl.GetRayAmp_p(), refl.GetRayAmp_s());
+	}
+
+	fclose(output);
 
 	return 0;
 }
