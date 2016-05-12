da4d28ccaeef380bf9ffafbf788f5149b86d55c1
Szymon <szymonlis@yahoo.com>
2016-05-12 14:55:53 +0100

    First simple version of user interface

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
-#include "gmock/gmock.h"
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include "ComplexNumber.h"
+#include "Ray.h"
+#include "Layer.h"
+#include "HitPoint.h"
 
-int main(int argc, char** argv)
+using namespace std;
+
+int main()
 {
-	testing::InitGoogleMock(&argc, argv);
-	return RUN_ALL_TESTS(); 
+
+	//stac of layers
+	Layer air(0, {1.0, 0.0}, 0.0);
+	Layer glass(1, {1.5, 0.0}, 1.0);;
+
+	//light beams
+	Ray laser(0.0, 1.0, 1.0, 0.0, 0.632, true, 0);
+	Ray refl;
+	Ray tran;
+
+	//interface
+	HitPoint hit(air, glass, laser, refl, tran);
+
+	printf("Angle:\tRp:\tRs:\t\n");
+	printf("%.5f %.5f %.5f\n", laser.GetRayAngle(), refl.GetRayAmp_p(), refl.GetRayAmp_p());
+
+	return 0;
 }
