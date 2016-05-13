8e773377bce2cea23409cd9f5f11d7140ed2eaed
Szymon <szymonlis@yahoo.com>
2016-05-13 19:27:57 +0100

    Loop over all transmiited beam was added

--- src/LayerStack.h
+++ src/LayerStack.h
@@ -17,6 +17,7 @@
 
 using namespace std;
 using namespace boost;
+
 class LayerStack {
 
 //constructor
@@ -46,8 +47,37 @@ LayerStack::~LayerStack() {
 //methods
 void LayerStack::Solve(ptr_vector<Layer> vStc, Ray& rIn, double& rRp, double& rRs) {
 
-	rRp = rIn.GetRayAmp_p();
-	rRs = rIn.GetRayAmp_s();
+	unsigned int N = vStc.size();
+
+	HitPoint hitPoint;
+
+	ptr_vector<Ray> vTran;		//vector with all transmitted beams
+	ptr_vector<Ray> vRefl;		//vector with reflected beam used to find Rp and Rs
+
+	vTran.push_back(new Ray{rIn});	//first elemnt is the incidence laser beam 
+
+	Ray tRefl;		//temporary Ray object
+	Ray tTran;		//temporary Ray object
+
+	//check if there any interface if not return 0.0
+	if(N<2) {
+		rRp = 0.0;
+		rRs = 0.0;
+		return;
+	}
+
+	//loop over all layers
+	for(unsigned int i=0; i<N-1; i++)
+	{
+		hitPoint.Solve(vStc[i], vStc[i+1], vTran[i], tRefl, tTran);
+		vTran.push_back(new Ray{tTran});	//add transmitted beam
+		vRefl.push_back(new Ray{tRefl});	//add reflected beam
+	}
+
+	rRp = vRefl[0].GetRayAmp_p();
+	rRs = vRefl[0].GetRayAmp_s();
+
+	return;
 }
 
 #endif
--- src/test/stackTest.cpp
+++ src/test/stackTest.cpp
@@ -19,33 +19,89 @@ public:
 	Layer si;
 
 	Ray laser;
+	Ray laser25deg;
 
-	double rp, rs;
+	double rpOne, rsOne, rpOne25deg, rsOne25deg;
+	double rpTwo, rsTwo, rpTwo25deg, rsTwo25deg;
+	double rpThree, rsThree, rpThree25deg, rsThree25deg;
 
-	LayerStack layerStack;
+	LayerStack singleLayer;
+	LayerStack twoLayers;
+	LayerStack threeLayers;
 
 //constructor:
 	OpticStack(): air(0, {1.0, 0.0}, 0.0),
 			glass(1, {1.5, 0.0}, 0.1),
-			si(2, {3.4, 0.4}, 1.0), 
-			laser(0.0, 1.0, 1.0, 0.0, 0.632, true, 0) {
+			si(2, {3.4, 0.4}, 1.0),
+			laser(0.0, 1.0, 1.0, 0.0, 0.632, true, 0),
+			laser25deg(25.0, 1.0, 1.0, 0.0, 0.632, true, 0) {
 
 		boost::ptr_vector<Layer> vStack;
-		vStack.push_back(new Layer{air});
-		vStack.push_back(new Layer{glass});
-		vStack.push_back(new Layer{si});
 
-		layerStack.Solve(vStack, laser, rp, rs);
+//first layer
+		vStack.push_back(new Layer{air});
+		singleLayer.Solve(vStack, laser, rpOne, rsOne);
+		singleLayer.Solve(vStack, laser25deg, rpOne25deg, rsOne25deg);
+
+//second layer
+		vStack.push_back(new Layer{glass});
+		twoLayers.Solve(vStack, laser, rpTwo, rsTwo);
+		twoLayers.Solve(vStack, laser25deg, rpTwo25deg, rsTwo25deg);
+
+//third layer
+		vStack.push_back(new Layer{si});
+		threeLayers.Solve(vStack, laser, rpThree, rsThree);
+		threeLayers.Solve(vStack, laser, rpThree25deg, rsThree25deg);
 };
 
 //deconstructor:
 	~OpticStack() {};
 };
 
-TEST_F(OpticStack, FirstTest) {
-	ASSERT_DOUBLE_EQ(rp, laser.GetRayAmp_p());
+TEST_F(OpticStack, ReflectionRp0degLayersLessThan2) {
+	ASSERT_DOUBLE_EQ(rpOne, 0.0);
 }
 
-TEST_F(OpticStack, See) {
-	ASSERT_DOUBLE_EQ(rs, laser.GetRayAmp_s());
+TEST_F(OpticStack, ReflectionRs0degLayersLessThan2) {
+	ASSERT_DOUBLE_EQ(rsOne, 0.0);
+}
+
+TEST_F(OpticStack, ReflectionRp25degLayersLessThan2) {
+	ASSERT_DOUBLE_EQ(rpOne25deg, 0.0);
+}
+
+TEST_F(OpticStack, ReflectionRs25degLayersLessThan2) {
+	ASSERT_DOUBLE_EQ(rsOne25deg, 0.0);
+}
+
+TEST_F(OpticStack, ReflectionRp0degTwoLayers) {
+	ASSERT_DOUBLE_EQ(rpTwo, 0.04);
+}
+
+TEST_F(OpticStack, ReflectionRs0degTwoLayers) {
+	ASSERT_DOUBLE_EQ(rsTwo, 0.04);
+}
+
+TEST_F(OpticStack, ReflectionRp25degTwoLayers) {
+	ASSERT_NEAR(rpTwo25deg, 0.0297493, 0.0000001);
+}
+
+TEST_F(OpticStack, ReflectionRs25degTwoLayers) {
+	ASSERT_NEAR(rsTwo25deg, 0.0516235, 0.0000001);
+}
+
+TEST_F(OpticStack, ReflectionRp0degThreeLayers) {
+	ASSERT_NEAR(rpThree, 0.04332, 0.00001);
+}
+
+TEST_F(OpticStack, ReflectionRs0degThreeLayers) {
+	ASSERT_NEAR(rsThree,  0.04332, 0.0001);
+}
+
+TEST_F(OpticStack, ReflectionRp25degThreeLayers) {
+	ASSERT_NEAR(rpThree25deg, 0.04452, 0.0001);
+}
+
+TEST_F(OpticStack, ReflectionRs25degThreeLayers) {
+	ASSERT_NEAR(rsThree25deg, 0.05109, 0.0001);
 }
