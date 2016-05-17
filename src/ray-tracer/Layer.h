9e93bbe56e4ab223005aeb0b02f3e42f9cd30d6c
Szymon <szymonlis@yahoo.com>
2016-05-17 14:36:31 +0100

    Cleaning the branch ray-tracer

---
+++ src/ray-tracer/Layer.h
@@ -0,0 +1,95 @@
+/* Copyright (C) 2016 Szymon Lis			email: szymonlis@yahoo.com
+ *							twitter: @drSzymonLis
+ *
+ * Class holding the physical layer object
+ *
+ * Layer is characterized by:
+ *	- complex refractive index
+ *	- thickness
+ *	- position number (counting from the top (air -> substrate),
+ *		where layer number 0 is assumed to be air)
+ *
+ * NOTE: If you set a layer at 0 postion the 0.0um thickness is forced
+ *
+ */
+
+#ifndef Layer_h
+#define Layer_h
+
+#include "ComplexNumber.h"
+
+class Layer {
+
+public:
+
+//constructors
+	Layer();
+	Layer(int, COMPLEX, double);
+	~Layer();
+
+//prototypes
+	COMPLEX GetRefractiveIndex() const;
+	void SetRefractiveIndex(COMPLEX);
+
+	int GetPosition() const;
+	void SetPosition(int);
+
+	double GetThickness() const;
+	void SetThickness(double);
+
+//members
+private:
+
+	COMPLEX refractiveIndex;	//refractive index
+	double thickness;		//thickness of layer
+	int position;			//number of layer on the layer stack
+};
+
+
+//definition of layer constructors
+Layer::Layer() {
+	position = 0;
+	refractiveIndex = {1.0, 0.0};
+	thickness = 0.0;
+}
+
+Layer::Layer(int x, COMPLEX y, double z) {
+	position = x;
+	refractiveIndex = y;
+	thickness = z;
+	if(position==0) { thickness=0.0; }	//keep the top layer on some 0.0 thickness
+}
+
+//deconstructor
+Layer::~Layer() {
+}
+
+
+//definition of methods
+COMPLEX Layer::GetRefractiveIndex() const {
+	return {refractiveIndex.real, refractiveIndex.imag};
+}
+
+void Layer::SetRefractiveIndex(COMPLEX x) {
+	refractiveIndex = x;
+}
+
+int Layer::GetPosition() const {
+	return position;
+}
+
+void Layer::SetPosition(int x) {
+	position = x;
+}
+
+double Layer::GetThickness() const {
+	return thickness;
+}
+
+void Layer::SetThickness(double x) {
+	thickness = x;
+	if(position==0) { thickness = 0.0; }	//to keep uniform thickness of top layer
+}
+
+
+#endif
---
+++ src/ray-tracer/Ray.h
@@ -0,0 +1,148 @@
+/* Copyright (C) 2016 Szymon Lis			email: szymonlis@yahoo.com
+ *							twitter: @drSzymonLis
+ *
+ * Class holding the light ray object
+ *
+ * Ray is characterised by:
+ *	- angle of ray propagation in reference to layer stack normal
+ *	- amp_p amplitude of 'p' type polarization
+ *	- amp_s amplitude of 's' type polarization
+ *	- phase of the ray [rad]
+ *	- wavelength of the ray [um]
+ *	- direction (true: for transmission towards substrate; false: towards air)
+ *	- layer where ray is at particular moment
+ *
+ */
+
+#ifndef Ray_h
+#define Ray_h
+
+
+#include "ComplexNumber.h"
+
+class Ray {
+
+public:
+
+//constructor prototypes
+	Ray();
+	Ray(double, double, double, double, double, bool, int);
+	~Ray();
+
+//prototypes
+	double GetRayAngle() const;
+	void SetRayAngle(double);
+
+	double GetRayAmp_p() const;
+	void SetRayAmp_p(double);
+
+	double GetRayAmp_s() const;
+	void SetRayAmp_s(double);
+
+	double GetRayPhase() const;
+	void SetRayPhase(double);
+
+	double GetRayWavelength() const;
+	void SetRayWavelength(double);
+
+	bool GetRayDirection() const;
+	void SetRayDirection(bool);
+
+	int GetRayLayer() const;
+	void SetRayLayer(int);
+
+//members
+private:
+
+	double angle;		//angle of propagation
+	double amp_p;		//'p' type polarization amplitude
+	double amp_s;		//'s' type polarization amplitude
+	double phase;		//phase [rad]
+	double wavelength;	//wavelength [um]
+	bool direction;		//true when ray travels towards substrate
+	int layer;		//number of layer where ray is
+};
+
+//definition of ray constructors
+Ray::Ray() {
+	angle = 0.0;
+	amp_p = 1.0;
+	amp_s = 1.0;
+	phase = 0.0;
+	wavelength = 1.0;
+	direction = true;
+	layer = 0;
+}
+
+Ray::Ray(double x, double p, double s, double y, double w, bool d, int l) {
+	angle = x;
+	amp_p = p;
+	amp_s = s;
+	phase = y;
+	wavelength = w;
+	direction = d;
+	layer = l;
+}
+
+//deconstructor
+Ray::~Ray() {
+}
+
+//definition of methods
+double Ray::GetRayAngle() const {
+	return angle;
+}
+
+void Ray::SetRayAngle(double x) {
+	angle = x;
+}
+
+double Ray::GetRayAmp_p() const {
+	return amp_p;
+}
+
+void Ray::SetRayAmp_p(double x) {
+	amp_p = x;
+}
+
+double Ray::GetRayAmp_s() const {
+	return amp_s;
+}
+
+void Ray::SetRayAmp_s(double x) {
+	amp_s = x;
+}
+
+double Ray::GetRayPhase() const {
+	return phase;
+}
+
+void Ray::SetRayPhase(double x) {
+	phase = x;
+}
+
+double Ray::GetRayWavelength() const {
+	return wavelength;
+}
+
+void Ray::SetRayWavelength(double x) {
+	wavelength = x;
+}
+
+bool Ray::GetRayDirection() const {
+	return direction;
+}
+
+void Ray::SetRayDirection(bool x) {
+	direction = x;
+}
+
+int Ray::GetRayLayer() const {
+	return layer;
+}
+
+void Ray::SetRayLayer(int x) {
+	layer = x;
+}
+
+#endif
